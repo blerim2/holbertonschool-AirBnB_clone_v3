5227d81b5e697487ca888cc1100170f8b885f84e
blerim2 <blerimballiu19@gmail.com>
2024-04-19 12:52:53 +0200

    update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
-""" Module to create API endpoints"""
+'''
+Modul to create API endpoints
+'''
 from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
-from flask import jsonify, request, abort
+from flask import jsonify
+from flask import request
 from api.v1.views import app_views
+from flask import abort
 
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['GET', 'POST'], strict_slashes=False)
 def get_places(city_id=None):
-    """func to return places"""
+    '''
+    Simple route
+    '''
     city = storage.get(City, city_id)
 
     if city is None:
         return abort(404)
     if request.method == 'GET':
-        places = []
-        for place in city.places:
-            places.append(place.to_dict())
+        places = [place.to_dict() for place in city.places]
         return jsonify(places)
     if request.method == 'POST':
         data = request.get_json(silent=True, force=True)
         if not data:
             return abort(400, description="Not a JSON")
-        if "user_id" not in data:
+        user_id = data.get("user_id", None)
+        place_name = data.get("name", None)
+        if user_id is None:
             return abort(400, description="Missing user_id")
-        user_id = data["user_id"]
         if storage.get(User, user_id) is None:
             return abort(404)
-        if "name" not in data:
+        if place_name is None:
             return abort(400, description="Missing name")
-        new_place = Place(**data)
+        new_place = Place(name=place_name, user_id=user_id, city_id=city_id)
         storage.new(new_place)
         storage.save()
         return jsonify(new_place.to_dict()), 201
@@ -41,7 +46,9 @@ def get_places(city_id=None):
 @app_views.route('/places/<place_id>',
                  methods=['DELETE', 'GET', 'PUT'], strict_slashes=False)
 def delete_place(place_id=None):
-    """func to delete objects"""
+    '''
+    Simple route
+    '''
     place = storage.get(Place, place_id)
     if place is None:
         return abort(404)
@@ -55,9 +62,9 @@ def delete_place(place_id=None):
         data = request.get_json(force=True)
         if not data:
             return abort(400, description="Not a JSON")
-        for key, value in data.items():
-            if key not in ['id', 'updated_at', 'created_at',
+        for k, v in data.items():
+            if k not in ['id', 'updated_at', 'created_at',
                          'user_id', 'city_id']:
-                setattr(place, key, value)
+                setattr(place, k, data[k])
         storage.save()
         return jsonify(place.to_dict()), 200
